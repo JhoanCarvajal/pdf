a0f39346e7b3046d5b50e39c2d8aa6ce5d8fe2f2
JhoanCarvajal <jhoans127@outlook.es>
2020-11-17 13:13:25 -0500

    borre carpetas innecesarias

--- funciones/dividir_roi.py
+++
@@ -1,9 +0,0 @@
-def dividir(roi):
-    rows,cols = roi.shape
-    mitad = cols / 2
-    mitad = int(mitad)
-    fecha_inicio = roi[0:rows,0:mitad-10]
-    fecha_final = roi[0:rows,mitad+4:cols]
-
-    return fecha_inicio, fecha_final
-    
