21d2bd3ec1405be9231930d8bebce257f2b90d15
JhoanCarvajal <jhoans127@outlook.es>
2020-12-16 15:45:15 -0500

    elimine unos archivos

--- crear_restaurante.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-from tkinter import simpledialog
-
-def nombre_restaurante():
-    root = tk.Toplevel()
-    root.withdraw()
-    nombre = simpledialog.askstring("Nuevo Restaurante", "Nombre del restaurante:")
-    return nombre
