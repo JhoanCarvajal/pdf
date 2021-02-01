2eb3e0dd273bdb396add3ac845de6c50d96651df
JhoanCarvajal <jhoans127@outlook.es>
2021-02-01 18:32:54 -0500

    intente detectar los bordes y asi recorrerlos para visualizar la informacion pero no me dio

--- detectar_proveedor.py
+++ detectar_proveedor.py
@@ -4,13 +4,34 @@ import pytesseract
 import os
 import controlador
 import ocr
+import imutils
 
 def proveedor(self, ruta):
     try:
         #cargamos la imagen contenida en ruta
+        rhh = cv2.imread(ruta)
         imagen = cv2.imread(ruta, 0)
         #transformamos a escala de grises
         image = 255 - cv2.threshold(imagen, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+        canny = cv2.Canny(image, 100, 200)
+        canny = cv2.dilate(canny, None, iterations=1)
+
+        contornos, h = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        for c in contornos:
+            area = cv2.contourArea(c)
+            x,y,w,h = cv2.boundingRect(c)
+            if area > 10000:
+                print(area)
+                cv2.drawContours(rhh, [c], 0, (0,255,0), 3)
+
+        img_resize = imutils.resize(rhh, width=600)
+        print("contornos =", len(contornos))
+        # for i in range(len(contornos)):
+        #     cv2.drawContours(img_resize, contornos, i, (0,255,0), 3)
+        #     cv2.imshow("imagen", img_resize)
+        #     cv2.waitKey(0)
 
         bool_solo_negro = None
         rois = []
@@ -27,6 +48,7 @@ def proveedor(self, ruta):
             titulo = str(i)
             cv2.imshow(titulo, roi)
             i+=1
+        cv2.imshow("resize", img_resize)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
